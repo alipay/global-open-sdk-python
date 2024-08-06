c5f14483430c5f99e0f293fd386f6c224963f2c9
ScottWryyyyy <zhangtianren.ztr@antgroup.com>
2024-08-06 20:36:00 +0800

    modify param name

--- com/alipay/ams/api/tools/webhook_tool.py
+++ com/alipay/ams/api/tools/webhook_tool.py
@@ -3,8 +3,8 @@ from com.alipay.ams.api.exception.exception import AlipayApiException
 from com.alipay.ams.api.tools.signature_tool import verify
 
 
-def check_signature(http_method, path, client_id, rsp_time_str, rsp_body, signature, alipay_public_key):
+def check_signature(http_method, path, client_id, req_time_str, req_body, signature, alipay_public_key):
     if signature and 'signature=' in signature:
         signature = signature.split('signature=')[1]
-        return verify(http_method, path, client_id, rsp_time_str, rsp_body, signature, alipay_public_key)
+        return verify(http_method, path, client_id, req_time_str, req_body, signature, alipay_public_key)
     raise AlipayApiException('signature invalid:' + str(signature))
