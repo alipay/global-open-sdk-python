d1fe53330ab6306b8c9e20d00ee707651c448eab
Wang Zunjiao <wangzunjiao.wzj@digital-engine.com>
2025-12-25 02:56:21 +0000

    false[antom-sdk-automation] automated change

--- com/alipay/ams/api/model/fund_move_detail.py
+++ com/alipay/ams/api/model/fund_move_detail.py
@@ -8,6 +8,8 @@ class FundMoveDetail:
         
         self.__memo = None  # type: str
         self.__reference_transaction_id = None  # type: str
+        self.__payer_asset_id = None  # type: str
+        self.__beneficiary_asset_id = None  # type: str
         
 
     @property
@@ -30,6 +32,26 @@ class FundMoveDetail:
     @reference_transaction_id.setter
     def reference_transaction_id(self, value):
         self.__reference_transaction_id = value
+    @property
+    def payer_asset_id(self):
+        """Gets the payer_asset_id of this FundMoveDetail.
+        
+        """
+        return self.__payer_asset_id
+
+    @payer_asset_id.setter
+    def payer_asset_id(self, value):
+        self.__payer_asset_id = value
+    @property
+    def beneficiary_asset_id(self):
+        """Gets the beneficiary_asset_id of this FundMoveDetail.
+        
+        """
+        return self.__beneficiary_asset_id
+
+    @beneficiary_asset_id.setter
+    def beneficiary_asset_id(self, value):
+        self.__beneficiary_asset_id = value
 
 
     
@@ -40,6 +62,10 @@ class FundMoveDetail:
             params['memo'] = self.memo
         if hasattr(self, "reference_transaction_id") and self.reference_transaction_id is not None:
             params['referenceTransactionId'] = self.reference_transaction_id
+        if hasattr(self, "payer_asset_id") and self.payer_asset_id is not None:
+            params['payerAssetId'] = self.payer_asset_id
+        if hasattr(self, "beneficiary_asset_id") and self.beneficiary_asset_id is not None:
+            params['beneficiaryAssetId'] = self.beneficiary_asset_id
         return params
 
 
@@ -50,3 +76,7 @@ class FundMoveDetail:
             self.__memo = response_body['memo']
         if 'referenceTransactionId' in response_body:
             self.__reference_transaction_id = response_body['referenceTransactionId']
+        if 'payerAssetId' in response_body:
+            self.__payer_asset_id = response_body['payerAssetId']
+        if 'beneficiaryAssetId' in response_body:
+            self.__beneficiary_asset_id = response_body['beneficiaryAssetId']
---
+++ com/alipay/ams/api/response/aba/alipay_update_card_response.py
@@ -0,0 +1,71 @@
+import json
+from com.alipay.ams.api.model.result import Result
+
+
+
+from com.alipay.ams.api.response.alipay_response import AlipayResponse
+
+class AlipayUpdateCardResponse(AlipayResponse):
+    def __init__(self, rsp_body):
+        super(AlipayResponse, self).__init__() 
+
+        self.__status = None  # type: str
+        self.__request_id = None  # type: str
+        self.__result = None  # type: Result
+        self.parse_rsp_body(rsp_body) 
+
+
+    @property
+    def status(self):
+        """Gets the status of this AlipayUpdateCardResponse.
+        
+        """
+        return self.__status
+
+    @status.setter
+    def status(self, value):
+        self.__status = value
+    @property
+    def request_id(self):
+        """Gets the request_id of this AlipayUpdateCardResponse.
+        
+        """
+        return self.__request_id
+
+    @request_id.setter
+    def request_id(self, value):
+        self.__request_id = value
+    @property
+    def result(self):
+        """Gets the result of this AlipayUpdateCardResponse.
+        
+        """
+        return self.__result
+
+    @result.setter
+    def result(self, value):
+        self.__result = value
+
+
+    
+
+    def to_ams_dict(self):
+        params = dict()
+        if hasattr(self, "status") and self.status is not None:
+            params['status'] = self.status
+        if hasattr(self, "request_id") and self.request_id is not None:
+            params['requestId'] = self.request_id
+        if hasattr(self, "result") and self.result is not None:
+            params['result'] = self.result
+        return params
+
+
+    def parse_rsp_body(self, response_body):
+        response_body = super(AlipayUpdateCardResponse, self).parse_rsp_body(response_body)
+        if 'status' in response_body:
+            self.__status = response_body['status']
+        if 'requestId' in response_body:
+            self.__request_id = response_body['requestId']
+        if 'result' in response_body:
+            self.__result = Result()
+            self.__result.parse_rsp_body(response_body['result'])
