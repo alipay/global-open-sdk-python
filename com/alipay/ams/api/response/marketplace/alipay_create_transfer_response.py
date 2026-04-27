d30671479ef954ba66a81f3eceb97fc22675061f
Wang Zunjiao <wangzunjiao.wzj@digital-engine.com>
2026-04-27 02:38:22 +0000

    false[antom-sdk-automation] automated change

--- com/alipay/ams/api/response/marketplace/alipay_create_transfer_response.py
+++ com/alipay/ams/api/response/marketplace/alipay_create_transfer_response.py
@@ -1,7 +1,7 @@
 import json
 from com.alipay.ams.api.model.result import Result
-from com.alipay.ams.api.model.transfer_from_detail import TransferFromDetail
-from com.alipay.ams.api.model.transfer_to_detail import TransferToDetail
+from com.alipay.ams.api.model.transfer_from_detail_response import TransferFromDetailResponse
+from com.alipay.ams.api.model.transfer_to_detail_response import TransferToDetailResponse
 
 
 
@@ -12,10 +12,10 @@ class AlipayCreateTransferResponse(AlipayResponse):
         super(AlipayResponse, self).__init__() 
 
         self.__result = None  # type: Result
-        self.__transfer_id = None  # type: str
         self.__transfer_request_id = None  # type: str
-        self.__transfer_from_detail = None  # type: TransferFromDetail
-        self.__transfer_to_detail = None  # type: TransferToDetail
+        self.__transfer_id = None  # type: str
+        self.__transfer_from_detail = None  # type: TransferFromDetailResponse
+        self.__transfer_to_detail = None  # type: TransferToDetailResponse
         self.parse_rsp_body(rsp_body) 
 
 
@@ -30,25 +30,25 @@ class AlipayCreateTransferResponse(AlipayResponse):
     def result(self, value):
         self.__result = value
     @property
-    def transfer_id(self):
-        """
-        The unique ID assigned by Antom to identify a transfer.  This parameter is returned when the value of result.resultStatus is U.   More information:  Maximum length: 64 characters
-        """
-        return self.__transfer_id
-
-    @transfer_id.setter
-    def transfer_id(self, value):
-        self.__transfer_id = value
-    @property
     def transfer_request_id(self):
         """
-        The unique ID assigned by the marketplace to identify a transfer request.  This parameter is returned when the value of result.resultStatus is U.   More information:  Maximum length: 64 characters
+        The unique ID assigned by the merchant to identify a transfer request. Note: This field is returned on demand only when the value of result.resultStatus is S.
         """
         return self.__transfer_request_id
 
     @transfer_request_id.setter
     def transfer_request_id(self, value):
         self.__transfer_request_id = value
+    @property
+    def transfer_id(self):
+        """
+        The unique ID assigned by Antom to identify a transfer. Note: This field is returned on demand only when the value of result.resultStatus is S.
+        """
+        return self.__transfer_id
+
+    @transfer_id.setter
+    def transfer_id(self, value):
+        self.__transfer_id = value
     @property
     def transfer_from_detail(self):
         """Gets the transfer_from_detail of this AlipayCreateTransferResponse.
@@ -77,10 +77,10 @@ class AlipayCreateTransferResponse(AlipayResponse):
         params = dict()
         if hasattr(self, "result") and self.result is not None:
             params['result'] = self.result
-        if hasattr(self, "transfer_id") and self.transfer_id is not None:
-            params['transferId'] = self.transfer_id
         if hasattr(self, "transfer_request_id") and self.transfer_request_id is not None:
             params['transferRequestId'] = self.transfer_request_id
+        if hasattr(self, "transfer_id") and self.transfer_id is not None:
+            params['transferId'] = self.transfer_id
         if hasattr(self, "transfer_from_detail") and self.transfer_from_detail is not None:
             params['transferFromDetail'] = self.transfer_from_detail
         if hasattr(self, "transfer_to_detail") and self.transfer_to_detail is not None:
@@ -93,13 +93,13 @@ class AlipayCreateTransferResponse(AlipayResponse):
         if 'result' in response_body:
             self.__result = Result()
             self.__result.parse_rsp_body(response_body['result'])
-        if 'transferId' in response_body:
-            self.__transfer_id = response_body['transferId']
         if 'transferRequestId' in response_body:
             self.__transfer_request_id = response_body['transferRequestId']
+        if 'transferId' in response_body:
+            self.__transfer_id = response_body['transferId']
         if 'transferFromDetail' in response_body:
-            self.__transfer_from_detail = TransferFromDetail()
+            self.__transfer_from_detail = TransferFromDetailResponse()
             self.__transfer_from_detail.parse_rsp_body(response_body['transferFromDetail'])
         if 'transferToDetail' in response_body:
-            self.__transfer_to_detail = TransferToDetail()
+            self.__transfer_to_detail = TransferToDetailResponse()
             self.__transfer_to_detail.parse_rsp_body(response_body['transferToDetail'])
